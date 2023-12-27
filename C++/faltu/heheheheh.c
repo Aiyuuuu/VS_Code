d936d2925c8728607fb9e0bfe7d724fb5b3f202c
Aiyuuuu <ayantahir360@gmail.com>
2023-12-28 00:39:04 +0500

    update!

---
+++ C++/faltu/heheheheh.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int arr1[4]={1,2,3,4};
+
+    int arr2[2][2]={(1,2),(3,4)};
+
+    printf("%d", arr1[3]);
+
+    printf("%d", arr2[2][2]);
+
+    return 0;
+}
+
+
---
+++ C++/faltu/rebel.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+
+
+int main()
+{       
+    // int arr[]={1,2,3,4,5,6,7,8,9,10};
+
+    // int arr2[]={1,2,3,4,5,6,7,8,9,10};
+
+    // int result[10]={0,0,0,0,0,0,0,0,0,0};
+
+
+
+
+
+
+    int arr1[]={1,2,3,4,5,6,7,8,9,10};
+
+    int arr2[]={1,2,3,4,5,6,7,8,9,10};
+
+        
+
+
+    for(int i=0; i<10; i++){
+        int num1=arr1[i];
+        int num2=arr2[i];
+        
+        cout<<num1+num2<<" ";
+    }
+
+
+
+
+    return 0;
+}
